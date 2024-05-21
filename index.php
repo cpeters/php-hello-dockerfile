271c156b99bc98022fef349d34a0ce8abd5ee015
Chris Peters <chrispeters.psu@gmail.com>
2024-05-21 18:41:08 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-echo '<body style="background-color:red">';
-print "Hello, Test GitOps.  Chris 123" . PHP_VERSION . "\n";
+echo '<body style="background-color:blue">';
+print "Hello, Test GitOps.  Chris 123  " . PHP_VERSION . "\n";
 ?>
