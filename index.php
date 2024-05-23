b20abf3d366fc37b31e3f116ac75e05b9c04d5ee
Chris Peters <chrispeters.psu@gmail.com>
2024-05-22 21:48:37 -0400

    Messing with index page

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html>
-<body style="background-color=#181818">
+<body style="background-color:powderblue;">
 <h1>Hello World</h1>
 
 <?php
-	print "Hello, Chris. Working from Dev Spaces  " . PHP_VERSION . "\n";
+	<!-- print "Hello, Chris. Working from Dev Spaces  " . PHP_VERSION . "\n"; -->
+	echo "Hello, Chris. Today is " . date("m-d-Y") . "<br>";
 ?>
 
 </body>
